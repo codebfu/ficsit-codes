340ab326267d831774b2e923d265a954f7f876f6
Gaultier HUBERT <gaultier.hubert@gmail.com>
2024-12-15 18:16:25 +0100

    well...

--- codes/init.lua
+++ codes/init.lua
@@ -1,4 +1,15 @@
+-- Configuration
 local realProgramName = "boot-client"
+local netBootPort = 1
+
+-- Setup Network
+local net = computer.getPCIDevices(classes.NetworkCard)[1]
+if not net then
+    error("Net-Boot: Failed to Start: No Network Card available!")
+end
+net:open(netBootPort)
+event.listen(net)
+
 local realProgram = nil
 while realProgram == nil do
     print("Net-Boot: Request Net-Boot-Program \"" .. realProgramName .. "\" from Port " .. netBootPort)
